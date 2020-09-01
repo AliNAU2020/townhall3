cc2dfbc59b38602219c6e540bd78dce5ca900dc2
AliNAU2020 <31328423+AliNAU2020@users.noreply.github.com>
2020-08-31 22:51:07 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php header ('Location: /index.html') ; ?>
+<?php header ('Location: /accordion_dialog.html') ; ?>
+<?php header ('Location: /gallery.html') ; ?>
+<?php header ('Location: /image_swaps.html') ; ?>
+<?php header ('Location: /image_rollovers.html') ; ?>
+<?php header ('Location: /lightbox.html') ; ?>
+<?php header ('Location: /slideshow.html') ; ?>
